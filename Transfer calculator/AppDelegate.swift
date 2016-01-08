f67258ca32eba47b971fd1e127908a988bf13002
Thomas Brichart <t.brichart@icloud.com>
2016-01-08 21:00:50 +0100

    Initial Commit

---
+++ Transfer calculator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Transfer calculator
+//
+//  Created by Thomas Brichart on 08/01/2016.
+//  Copyright © 2016 Thomas Brichart. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Transfer calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Transfer calculator
+//
+//  Created by Thomas Brichart on 08/01/2016.
+//  Copyright © 2016 Thomas Brichart. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
